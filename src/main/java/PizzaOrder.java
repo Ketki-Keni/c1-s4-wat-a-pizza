652866106aad40509da72739bc78cb8e4f02cbcd
Ketki Keni <sketki220@gmail.com>
2022-10-20 17:08:07 +0530

    declared variables to get input from the user using scanner

--- src/main/java/PizzaOrder.java
+++ src/main/java/PizzaOrder.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class PizzaOrder {
     public static void main(String[] args) {
+        //Task #1
+        Scanner scanner = new Scanner(System.in);
+        String customerName = scanner.next();
+        String customerEmail = scanner.next();
+        long phoneNo = scanner.nextLong();
+        String address = scanner.next();
+        float pizzaPriceIn$ = scanner.nextFloat();
+        char pizzaSize = scanner.next().charAt(0);
+        float garlicBreadPriceIn$ = scanner.nextFloat();
+        float beveragePriceIn$ = scanner.nextFloat();
 
     }
 }
